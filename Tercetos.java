8ebce8cadc419de602c846ccd02aed4a21bec45b
IradDV <104461790+IradDV@users.noreply.github.com>
2024-10-11 01:51:22 -0600

    Tercetos y main configurado para correr todo

--- Tercetos.java
+++ Tercetos.java
@@ -4,17 +4,67 @@ public class Tercetos {
 
     String[] operadores = {"+", "-", "*", "/"}; // Operadores válidos
     String[] palabras; // Cadena inicial separada por palabras
+    boolean tipoOperacion;
     int contTemp = 0; // Contador de temporales
     ArrayList<String> listaTercetos = new ArrayList<>(); // Almacenar los tercetos generados
 
     public Tercetos(String cadena) {
         // Dividimos la cadena en palabras usando split (por espacios)
-        this.palabras = cadena.split(" ");
+        //System.out.println("Consultamos la cadena "+cadena);
+        Delacarion(cadena.charAt(0));
+        if(Delacarion(cadena.charAt(0)) == 1){
+            this.tipoOperacion = true;
+            this.palabras = cadena.split(" ");
+        }
+        if (Delacarion(cadena.charAt(0)) == 2){
+            this.tipoOperacion = false;
+            this.palabras = cadena.split(",");   
+        }
     }
 
     // Procedimiento para generar los tercetos recursivamente
     void ProcedimientoPre() {
-        // Mientras haya operadores en la cadena
+        //vemos que procedimiento realizamos
+       if (tipoOperacion) {
+        //haremos el tipo normal 
+        //System.out.println("Analizaremos " + String.join(" ", palabras));
+        String op1 = "__", op2 = "__", op = "__";
+        boolean encontradoOperador = false;
+        
+        for (String s : palabras) {
+            // Leemos la cadena
+            //System.out.println("Cadena analizando -> " + s);
+            
+            // Verificar si es un operador
+            if (s.equals("=")) {
+                op = s; // Asignamos el operador
+                encontradoOperador = true;
+            } 
+            // Si ya se encontró un operador, lo que sigue son los operandos
+            else if (encontradoOperador) {
+                if (op2.equals("__")) {
+                    op2 = s; // Asignamos el segundo operando
+                }
+            }
+            // Si aún no se ha encontrado un operador, estamos con el primer operando
+            else {
+                if (op1.equals("__")) {
+                    op1 = s; // Asignamos el primer operando
+                }
+            }  
+        }
+        //metemos la segunda tercetos si es que op2 sigue vacio despues de forin
+        if (op2.equals("__")) {
+            op2 = palabras[palabras.length - 1];
+        }
+        //System.out.println("Operador: " + op);
+        //System.out.println("Primer operando: " + op1);
+        //System.out.println("Segundo operando: " + op2);
+        //System.out.println("Imprimios el terceto");
+        System.out.println(imprimirTerceto(op, op1, op2));
+       }
+       else {
+        //haremos el tipo de operacion
         while (contieneOperadores()) {
             for (int i = 0; i < palabras.length - 2; i++) {  // -2 para evitar desbordamientos
                 String palabra = palabras[i];
@@ -41,9 +91,10 @@ public class Tercetos {
                 }
             }
         }
-
         // Imprimir la cadena final
         System.out.println("Cadena final: " + String.join(" ", palabras));
+        System.out.println(imprimirTerceto(palabras[0], palabras[1], palabras[2]));
+        }
     }
 
     // Método auxiliar para generar el terceto
@@ -51,7 +102,10 @@ public class Tercetos {
         contTemp++; // Incrementar el contador de temporales
         return "Temp" + contTemp;
     }
-
+   
+    String imprimirTerceto(String operador, String operando1, String operando2) {
+        return "(" + operador + " " + operando1 + " " + operando2 + ")";
+    }
     // Método para verificar si una palabra es un operador
     boolean esOperador(String s) {
         for (String op : operadores) {
@@ -94,10 +148,15 @@ public class Tercetos {
         // Actualizar el arreglo de palabras
         palabras = nuevaCadena.toArray(new String[0]);
     }
-
-    public static void main(String[] args) {
-        // Cadena de prueba
-        Tercetos t = new Tercetos("= d * a b c d");
-        t.ProcedimientoPre(); // Ejecutar el procedimiento para generar tercetos
+    
+    //Metodo para saber si es declaracion o operacion
+    int Delacarion(char c){
+        if (c != '=') {
+            //System.out.println("Declaracion");
+            return 1;
+        } else {
+            //System.out.println("Operacion");
+            return 2;
+        }
     }
 }
--- main.java
+++ main.java
@@ -6,17 +6,30 @@ public class main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("cadenas.txt"))) {// lee el archivo
             Lexico l = new Lexico();// crea un objeto del analizador sintactico
-
             String line;// lee una línea
             while ((line = br.readLine()) != null) {// mientras aún haya líneas
                 l.analisisL(line);// realiza un análisis léxico-sintáctico a la cadena recibida
             }
-            l.printT();
-            Sintactico s = new Sintactico(l.getTokens());// crea un objeto del analizador sintactico
-            s.analisisS();
+             l.printT();
+
+            // Sintactico s = new Sintactico(l.getTokens());// crea un objeto del analizador
+            // sintactico
+            // s.analisisS();
 
             // Cerrar el PrintWriter al finalizar
-            s.writer.close();
+            // s.writer.close();
+            // le pasamos las lineas por lineas y el programa las guardara para despues su
+            // captura
+            System.out.println("Tercetos\n****************************************************************************");
+            BufferedReader br2 = new BufferedReader(new FileReader("Posfija.txt"));
+            String linea;
+            while ((linea = br2.readLine()) != null) {
+                System.out.println(linea);
+                Tercetos t = new Tercetos(linea);
+                t.ProcedimientoPre();
+            }
+            br2.close();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
